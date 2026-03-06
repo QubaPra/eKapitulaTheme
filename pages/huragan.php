816cae17c4fb357e6a7420ff26bf0d7d6f741faa
QubaPra <116064097+QubaPra@users.noreply.github.com>
2026-03-06 18:12:34 +0100

    cdn to raw githack

--- pages/huragan.php
+++ pages/huragan.php
@@ -1,8 +1,7 @@
 <?php
 // Pobieramy najnowszą wersję index.html bezpośrednio z repo GitHub'a.
 // branch `master` można zastąpić `master` w zależności jak nazywa się główna gałąź w repozytorium.
-// Używamy cdn.jsdelivr.net, aby zapewnić poprawne nagłówki i uniknąć limitów rate-limit GitHub API.
-$url = 'https://cdn.jsdelivr.net/gh/QubaPra/huraganGra@master/index.html';
+$url = 'https://raw.githack.com/QubaPra/huraganGra/master/index.html';
 
 // Pobieranie tresci
 $html_content = @file_get_contents($url);
@@ -11,7 +10,7 @@ if ($html_content !== false) {
     // Od razu po znaczniku <head> dodajemy `<base url>`, by wszystkie zasoby: css, js, json, obrazki
     // mogły poprawnie i automatycznie pobierać się z GitHuba, omijając WordPressa.
     // Dzięki temu nie ma problemów, że /styles.css będzie szukane w katalogu publicznym serwera krowodrza.zhr.pl.
-    $base_tag = "\n    <base href=\"https://cdn.jsdelivr.net/gh/QubaPra/huraganGra@master/\">\n";
+    $base_tag = "\n    <base href=\"https://raw.githack.com/QubaPra/huraganGra/master/\">\n";
     $html_content = preg_replace('/<head>/i', '<head>' . $base_tag, $html_content, 1);
     
     // Wypuszczamy zawartość
