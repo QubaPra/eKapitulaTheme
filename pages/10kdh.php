cc8d6ab791bdd20197a6e860c4b6b5a956db1da1
QubaPra <116064097+QubaPra@users.noreply.github.com>
2026-09-08 18:03:29 +0200

    10kdh github hosted

---
+++ pages/10kdh.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__DIR__) . '/github_page_loader.php';
+
+load_github_page(
+    'domek123', 
+    '10KDH', 
+    'main', 
+    'index.html', 
+    '10 KDH Dzieci Słońca - Strona Niedostępna'
+);
